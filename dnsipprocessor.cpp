8005d0162e34796465b3a280814c60ba287ad162
Ihor Dutchak <ihor.youw@gmail.com>
2014-12-04 06:11:33 +0200

    Minor fix

--- dnsipprocessor.cpp
+++ dnsipprocessor.cpp
@@ -9,7 +9,7 @@ DNSIPProcessor::DNSIPProcessor(const NameList& allowed_dns_names) :
 	allowed_names(allowed_dns_names)
 {
 #ifndef WIN32
-	system(("/etc/route_by_dns/startup.sh").c_str());
+	system("/etc/route_by_dns/startup.sh");
 #else
 	std::cout << "/etc/route_by_dns/startup.sh";
 #endif
